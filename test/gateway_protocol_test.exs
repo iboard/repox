3111eb2873def10f018f59d804b103612ea7346e
Andreas Altendorfer <andreas@altendorfer.at>
2015-05-07 10:20:52 +0200

    Prepared for testing more Gateway implementation

--- test/gateway_protocol_test.exs
+++ test/gateway_protocol_test.exs
@@ -1,21 +1,34 @@
 defmodule GatewayTest do
   use ExUnit.Case
 
-  test "get all entries as a list" do
-    gw = %ListGateway{}
-    assert [] == Gateway.to_list(gw)
+  setup do
+    all = [ 
+      %{id: 1, value: "one"}, 
+      %{id: 2, value: "two"},
+      %{id: 3, value: "three"} ]
+    empty  = [ %ListGateway{} ]
+    filled = [ %ListGateway{ entries: all } ]
+    {:ok, data: all, empty: empty, filled: filled}
   end
 
-  test "put something to the list" do
-    gw = Gateway.put(%ListGateway{}, %{key: :value})
-    assert [%{key: :value}] == Gateway.to_list(gw)
+
+  test "get all entries as a list", context do
+    Enum.each context[:empty], fn(gw) ->
+      assert [] == Gateway.to_list(gw)
+    end
   end
 
-  test "filter entries" do
-    all = [ %{id: 1, value: "one"}, %{id: 2, value: "two"},
-            %{id: 3, value: "three"} ]
-    expected = Enum.take(all, 2)
-    gw = %ListGateway{ entries: all }
-    assert expected == Gateway.filter( gw, &(&1.id <= 2) )
+  test "put something to the list", context do
+    Enum.each context[:empty], fn(gw) ->
+      new_gw = Gateway.put(gw, %{key: :value})
+      assert [%{key: :value}] == Gateway.to_list(new_gw)
+    end
+  end
+
+  test "filter entries", context do
+    expected = Enum.take(context[:data], 2)
+    Enum.each context[:filled], fn(gw) ->
+      assert expected == Gateway.filter( gw, &(&1.id <= 2) )
+    end
   end
 end
