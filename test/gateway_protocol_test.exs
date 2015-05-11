03a8a61c37d6516f00b2c1a2a57eb090cd41ac47
Andreas Altendorfer <andreas@altendorfer.at>
2015-05-11 12:01:12 +0200

    Reviewed test

--- test/gateway_protocol_test.exs
+++ test/gateway_protocol_test.exs
@@ -6,46 +6,51 @@ defmodule GatewayTest do
   @filled_testfile "/tmp/#{__MODULE__}.filled.json"
 
   setup do
-    all = [
-      %{id: 1, value: "one"},
-      %{id: 2, value: "two"},
-      %{id: 3, value: "three"} ]
+    seed = [
+             %{id: 1, value: "one"},
+             %{id: 2, value: "two"},
+             %{id: 3, value: "three"}
+           ]
 
-    # encode and write files
-    empty_json  = Poison.Encoder.encode([], [])
-    filled_json = Poison.Encoder.encode(all, [])
-    File.write!(@filled_testfile, filled_json)
-    File.write!(@empty_testfile, empty_json)
+    compose_test_files seed
 
     # return empty and filled gateways of both kinds
-    # Tests will use all gateways in empty and filled,
-    # thus the same test ensures all gateways behaves the same way
+    # Tests will use seed gateways in empty and filled,
+    # thus the same test ensures seed gateways behaves the same way
     empty  = [ %ListGateway{},
                %FileGateway{path: @empty_testfile} ]
-    filled = [ %ListGateway{entries: all},
+    filled = [ %ListGateway{entries: seed},
                %FileGateway{path: @filled_testfile} ]
 
-    {:ok, data: all, empty: empty, filled: filled}
+    {:ok, empty: empty, filled: filled, seed: seed}
   end
 
 
-  test "get all entries as a list", context do
+  test "Gateway.to_list(gw) returns all entries as a list", context do
     Enum.each context[:empty], fn(gw) ->
       assert [] == Gateway.to_list(gw)
     end
   end
 
-  test "put something to the list", context do
+  test "Gateway.put(gw,entry) put something into the gateway", context do
     Enum.each context[:empty], fn(gw) ->
       new_gw = Gateway.put(gw, %{key: 'value'})
       assert [%{key: 'value'}] == Gateway.to_list(new_gw)
     end
   end
 
-  test "filter entries", context do
-    expected = Enum.take(context[:data], 2)
+  test "Gateway.filter(gw,fn) filters entries with a function", context do
+    expected = Enum.take(context[:seed], 2)
     Enum.each context[:filled], fn(gw) ->
       assert expected == Gateway.filter( gw, &(&1.id <= 2) )
     end
   end
+
+  defp compose_test_files seed do
+    empty_json  = Poison.Encoder.encode([], [])
+    filled_json = Poison.Encoder.encode(seed, [])
+    File.write!(@filled_testfile, filled_json)
+    File.write!(@empty_testfile, empty_json)
+  end
+
 end
