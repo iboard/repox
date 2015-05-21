8312ba2dba17e09259c5b9dc9106bb261a17ea70
Andreas Altendorfer <andreas@altendorfer.at>
2015-05-21 09:57:59 +0200

    Minor review of benchmarks

--- bench/benchmarks_bench.exs
+++ bench/benchmarks_bench.exs
@@ -1,6 +1,7 @@
 defmodule BenchmarksBench do
 
   @test_file  "/tmp/#{__MODULE__}.bench"
+  @batch_size 10
 
   use Benchfella
   require Mongo.Bang
@@ -11,25 +12,25 @@ defmodule BenchmarksBench do
     {:ok, agent}
   end
 
-  bench "100x put to a Mongo collection", [gw: gen_mongo_gw()] do
-    BenchMacros.add_to_gateway(gw, 100)
+  bench "10x put to a Mongo collection", [gw: gen_mongo_gw()] do
+    BenchMacros.add_to_gateway(gw, @batch_size)
   end
 
-  bench "100x put to a File List", [gw: gen_file_gw] do
-    BenchMacros.add_to_gateway(gw, 100)
+  bench "10x put to a File List", [gw: gen_file_gw] do
+    BenchMacros.add_to_gateway(gw, @batch_size)
   end
 
-  bench "100x put to a Memory List", [gw: gen_list_gw] do
-    BenchMacros.add_to_gateway(gw, 100)
+  bench "10x put to a Memory List", [gw: gen_list_gw] do
+    BenchMacros.add_to_gateway(gw, @batch_size)
   end
 
-  defp gen_list_gw do %ListGateway{} end
-  defp gen_file_gw do %FileGateway{path: @test_file} end
-  defp gen_mongo_gw do
-    mongo = Repox.config(:mongo)
-    %MongoDBGateway{
-      db: "bench", collection: "bench_collection", mongo: mongo
-    }
-  end
+  defp gen_list_gw  do %ListGateway{ }                  end
+  defp gen_file_gw  do %FileGateway{ path: @test_file } end
+  defp gen_mongo_gw do %MongoDBGateway{
+                         db: "bench",
+                         collection: "bench_collection",
+                         mongo: Repox.config(:mongo)
+                       }
+                    end
 
 end
